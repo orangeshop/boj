ce19fdc43e726c6da44dae8898c8d9ef6d5f6809
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-03-20 17:22:47 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by 최영호 on 2021/12/16.
+//
+
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <memory.h>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <math.h>
+#include <set>
+#include <map>
+#include <iterator>
+
+using namespace std;
+#define S second
+#define F first
+#define l long
+#define MAX 0x7ffffff
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,1,0,-1};
+int ddx[8] = {-1,-2,-2,-1,1,2,2,1};
+int ddy[8] = {-2,-1,1,2,-2,-1,1,2};
+int cdx[8] = {-1,0,1,0,-1,-1,1,1};
+int cdy[8] = {0,1,0,-1,-1,1,1,-1};
+
+//cin.tie(NULL);
+//ios::sync_with_stdio(false);
+//cout.tie(NULL);
+int n,m;
+
+int main()
+{
+    cin >> n;
+    cin >> m;
+    
+    cout << n + m << endl;
+    
+    
+}

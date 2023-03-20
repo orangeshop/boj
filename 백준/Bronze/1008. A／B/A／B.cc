661be6e05ba8e0747c05c35567d75d36909bd7b3
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-03-20 17:09:25 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int a=0;
+    int b=0;
+    
+    cin >> a >> b;
+    
+    
+    
+    
+    
+    
+    cout.precision(10);
+    cout << a/(double)b;
+    
+}

f511f88cc25450a777645521374bb762af0c698d
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-03-20 17:20:40 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <memory.h>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <math.h>
+#include <list>
+#include <set>
+#include <map>
+#include <iterator>
+
+using namespace std;
+#define S second
+#define F first
+#define l long
+#define MAX 0x7ffffff
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,1,0,-1};
+int ddx[8] = {-1,-2,-2,-1,1,2,2,1};
+int ddy[8] = {-2,-1,1,2,-2,-1,1,2};
+int cdx[8] = {-1,0,1,0,-1,-1,1,1};
+int cdy[8] = {0,1,0,-1,-1,1,1,-1};
+
+//cin.tie(NULL);
+//ios::sync_with_stdio(false);
+//cout.tie(NULL);
+
+int main()
+{
+    long long n=0;
+    long long m=0;
+    
+    cin >> n >> m;
+    l l result =0;
+    result = n-m;
+    if(result < 0)
+    {
+        result *= -1;
+    }
+    
+    cout << result << endl;
+}

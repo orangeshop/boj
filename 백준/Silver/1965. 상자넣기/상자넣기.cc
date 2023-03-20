2368f144221f6aae73edf780307bed0de3ec67c3
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-03-20 17:16:27 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+using namespace std;
+
+
+int s[1005];
+int dp[1005];
+int n;
+
+int main()
+{
+    cin >> n;
+    int num=0;
+    for(int i=1; i<=n; i++)
+    {
+        cin >> s[i];
+    }
+    dp[1] = 1;
+    for(int i=2; i<=n; i++)
+    {
+        int cnt =0;
+        for(int k=i-1; k>=1; k--)
+        {
+            if(s[i] > s[k])
+            {
+                cnt = max(cnt, dp[k]);
+            }
+        }
+        dp[i] = cnt+1;
+        num = max(dp[i], num);
+        
+    }
+  
+   
+    cout << num << endl;
+ 
+}

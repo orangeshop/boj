93688d4fb88e9161f7af7a799f930e1903a9979b
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-03-20 17:22:37 +0900

    [Bronze V] Title: Hello World, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!";
+    
+    
+}

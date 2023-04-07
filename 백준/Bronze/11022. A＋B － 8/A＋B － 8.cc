adac8e5c2024c1eba9ebf7f80ca7c548e65a0e56
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:21:50 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    
+    int a=0;
+    scanf("%d", &a);
+    
+    for(int i=1; i< a+1; i++)
+    {
+        int b;
+        int c;
+        scanf(" %d", &b);
+        scanf("%d", &c);
+        cout << "Case #"<<i<<": "<< b<< " + "<<c<<" = "<<b+c << endl;
+    }
+
+}

adc097e59e926aa0c15bbb06516b45bf822c0186
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:27:49 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 372 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    
+    int a=0;
+    scanf("%d", &a);
+    
+    for(int i=0; i< a; i++)
+    {
+        int b;
+        int c;
+        scanf("%d", &b);
+        scanf("%d", &c);
+        printf("%d\n",b+c);
+    }
+
+}

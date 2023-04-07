b4221cd504ed46717847c92185348e65f91f8c94
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:20:14 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int a=0;
+    int b=0;
+    
+    cin >> a >> b;
+    
+    
+    
+    
+    cout << a+b << endl;
+    cout << a-b << endl;
+    cout << a*b << endl;
+    
+    cout << a/b << endl;
+    cout << a%b << endl;
+    
+}

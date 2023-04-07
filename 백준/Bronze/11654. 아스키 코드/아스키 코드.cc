b415bc591605749fff83d1bc376402efd13cf408
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:24:02 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    char a;
+    
+    cin >> a;
+    
+    cout << (int)a;
+    
+    
+}

e9650ca4a869f5d596117c9e2cd7bf268dbf48ab
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:14:01 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include<cstring>
+#include <vector>
+#include <stack>
+#include <memory.h>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <math.h>
+#include <set>
+#include <map>
+using namespace std;
+#define S second
+#define F first
+#define l long
+#define MAX 0x7fffffff
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
+//cin.tie(NULL);
+//ios::sync_with_stdio(false);
+int main()
+{
+    cout << "147" << endl;
+    cout << "rhkeh123" << endl;
+}

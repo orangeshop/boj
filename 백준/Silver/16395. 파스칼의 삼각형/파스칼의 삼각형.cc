c2996563927e9f193183195c5104988853a29949
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:29:58 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.cc
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <memory.h>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <math.h>
+#include <list>
+#include <set>
+#include <map>
+#include <iterator>
+
+using namespace std;
+#define S second
+#define F first
+#define l long
+#define MAX 0x7ffffff
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,1,0,-1};
+int ddx[8] = {-1,-2,-2,-1,1,2,2,1};
+int ddy[8] = {-2,-1,1,2,-2,-1,1,2};
+int cdx[8] = {-1,0,1,0,-1,-1,1,1};
+int cdy[8] = {0,1,0,-1,-1,1,1,-1};
+
+//cin.tie(NULL);
+//ios::sync_with_stdio(false);
+//cout.tie(NULL);
+
+
+int n;
+int m;
+
+int dp[34][34];
+
+
+int main()
+{
+    cin >> n >> m;
+    
+    dp[0][0] = 1;
+    
+    for(int i=1; i<n; i++)
+    {
+        for(int k=0; k<=i; k++)
+        {
+            if(k-1 < 0)
+            {
+                dp[i][k] = dp[i-1][k];
+                continue;
+            }
+            dp[i][k] = dp[i-1][k] + dp[i-1][k-1];
+            
+        }
+    }
+    
+    for(int i=0; i<n; i++)
+    {
+        for(int k=0; k<n; k++)
+        {
+            //cout << dp[i][k] << " ";
+            if(i==n-1 && k == m-1)
+            {
+                cout << dp[i][k] << endl;
+            }
+            
+        }
+        //cout << endl;
+    }
+    
+}

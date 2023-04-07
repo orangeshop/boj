7b4f5c1247b8b039e088204a587461ad4790c2f2
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:08:23 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include<cstring>
+#include <vector>
+#include <stack>
+#include <memory.h>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <math.h>
+#include <set>
+#include <map>
+using namespace std;
+#define S second
+#define F first
+#define l long
+#define MAX 0x7ffffff
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int ddx[8] = {-1,-2,-2,-1,1,2,2,1};
+int ddy[8] = {-2,-1,1,2,-2,-1,1,2};
+
+//cin.tie(NULL);
+//ios::sync_with_stdio(false);
+int main()
+{
+    string str ="";
+    cin >> str;
+    cout << str.length() << endl;
+}

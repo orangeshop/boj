f42d1a4561c11002af63adef83e48ef9d28461be
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:21:08 +0900

    [Bronze V] Title: A+B - 4, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    
+    int b=0;
+    int c=0;
+    while(true)
+    {
+        
+        cin >> b >> c;
+        if (cin.eof()) break;
+        cout << b+c << endl;
+        
+    }
+}
+    

472f0981d558cdedb694ac0bd59bacb253c676bc
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:19:21 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    cout << "강한친구 대한육군" << endl;
+    cout << "강한친구 대한육군";
+    
+}

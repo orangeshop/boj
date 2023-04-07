38c5ed5e91a70bcd436ff3e37631b8684618e6b7
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:10:29 +0900

    [Bronze V] Title: 크냐?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <memory.h>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <math.h>
+#include <set>
+#include <map>
+#include <iterator>
+
+using namespace std;
+#define S second
+#define F first
+#define l long
+#define MAX 0x7ffffff
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,1,0,-1};
+int ddx[8] = {-1,-2,-2,-1,1,2,2,1};
+int ddy[8] = {-2,-1,1,2,-2,-1,1,2};
+int cdx[8] = {-1,0,1,0,-1,-1,1,1};
+int cdy[8] = {0,1,0,-1,-1,1,1,-1};
+
+//cin.tie(NULL);
+//ios::sync_with_stdio(false);
+//cout.tie(NULL);
+
+int n,m;
+
+int main()
+{
+    while(1)
+    {
+        cin >> n >> m;
+        if(n== 0 && m==0) break;
+        if(n <= m)
+        {
+            cout << "No" << endl;
+        }
+        else{
+            cout << "Yes" << endl;
+        }
+    }
+}

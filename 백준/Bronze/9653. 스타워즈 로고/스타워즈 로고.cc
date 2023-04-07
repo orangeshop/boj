c6ce86f3d19a3973b7dc66bdbceb40c7d5a0b7f2
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:17:25 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <memory.h>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <math.h>
+#include <set>
+#include <map>
+#include <iterator>
+
+using namespace std;
+#define S second
+#define F first
+#define l long
+#define MAX 0x7ffffff
+int dx[4] = {-1,0,1,0};
+int dy[4] = {0,1,0,-1};
+int ddx[8] = {-1,-2,-2,-1,1,2,2,1};
+int ddy[8] = {-2,-1,1,2,-2,-1,1,2};
+
+//cin.tie(NULL);
+//ios::sync_with_stdio(false);
+
+int main()
+{
+    cout << "    8888888888  888    88888" << endl;
+    cout << "   88     88   88 88   88  88" << endl;
+    cout << "    8888  88  88   88  88888" << endl;
+    cout << "       88 88 888888888 88   88" << endl;
+    cout << "88888888  88 88     88 88    888888" << endl;
+    cout << "" << endl;
+    cout << "88  88  88   888    88888    888888" << endl;
+    cout << "88  88  88  88 88   88  88  88" << endl;
+    cout << "88 8888 88 88   88  88888    8888" << endl;
+    cout << " 888  888 888888888 88  88      88" << endl;
+    cout << "  88  88  88     88 88   88888888" << endl;
+    
+    
+}

741df9056270c4035f7985a34cb393b46e6ea111
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:31:40 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include<cstring>
+#include <vector>
+#include <stack>
+#include <memory.h>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <math.h>
+#include <set>
+#include <map>
+using namespace std;
+//cin.tie(NULL);
+//ios::sync_with_stdio(false);
+
+int n;
+string str = "____";
+
+void repeat(int a)
+{
+    for(int i=0; i<a; i++)
+    {
+        cout<< str;
+    }
+}
+
+void func(int a)
+{
+    if(a==n)
+    {
+        repeat(a);
+        cout << "\"재귀함수가 뭔가요?\"" << endl;
+        repeat(a);
+        cout << "\"재귀함수는 자기 자신을 호출하는 함수라네\"" << endl;
+        repeat(a);
+        cout << "라고 답변하였지." << endl;
+        return;
+    }
+    repeat(a);
+    cout << "\"재귀함수가 뭔가요?\"" <<endl;
+    repeat(a);
+    cout << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어." <<endl;
+    repeat(a);
+    cout << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지." <<endl;
+    repeat(a);
+    cout << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"" << endl;
+    
+    func(a+1);
+    
+    repeat(a);
+    cout << "라고 답변하였지." << endl;
+    
+}
+
+int main()
+{
+    cin >> n;
+    
+    cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다." << endl;
+    
+    func(0);
+    
+    
+}

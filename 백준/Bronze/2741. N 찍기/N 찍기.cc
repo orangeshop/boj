47e3d72acfb8a9e5ac86d2ab3d8472e2e6beea63
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:08:01 +0900

    [Bronze V] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    
+    int a=0;
+    cin >> a;
+    
+    for(int i=1; i< a+1; i++)
+    {
+        printf("%d\n", i);
+    }
+
+}

3693ec472ae938e24920421e0d799a8d2d042c44
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:18:38 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    cout << "\\    /\\" << endl;
+    cout << " )  ( ')" << endl;
+    cout << "(  /  )" << endl;
+    cout << " \\(__)|" << endl;
+    
+}

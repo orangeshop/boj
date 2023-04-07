6de852ebaf8e3277388dcd544255d60799595bbd
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:27:58 +0900

    [Silver III] Title: N과 M (1), Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+
+bool isused[10];
+int m,n;
+int arr[10];
+void dfs(int a)
+{
+    if(n==a)
+    {
+        for(int i=0; i< n; i++)
+        {
+            cout << arr[i]<< " " ;
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i=1; i<= m; i++)
+    {
+        if(!isused[i])
+        {
+            arr[a]=i;
+            isused[i]=1;
+            dfs(a+1);
+            isused[i]=0;
+        }
+    }
+}
+int main()
+{
+    cin >> m >> n;
+    dfs(0);
+}

8cbd485fb03f268e5734f6cefec1c553ba6f240d
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:21:19 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    
+    
+    while(true)
+    {
+        int b=0;
+        int c=0;
+        
+        cin >> b >>c;
+        
+        if(b==0 && c==0) break;
+        cout << b+c << endl;;
+        
+        
+    }
+}

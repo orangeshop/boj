5b5c94da83ad36299fab2783cf2963fadd022d83
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:20:58 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int a=0;
+    cin >> a;
+    
+    for(int i=0; i< a; i++)
+    {
+        int b=0;
+        int c=0;
+        
+        cin >> b >> c;
+        
+        cout << b+c << endl;
+    }
+
+}

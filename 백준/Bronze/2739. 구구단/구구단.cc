13a7e31fbe49a4820e06229f3ebb4b4e9cff8969
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:07:51 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int a=0;
+    cin >> a;
+    
+    for(int i=1; i< 10; i++)
+    {
+        cout << a << " * " << i << " = " << a*i << endl;;
+    }
+
+}

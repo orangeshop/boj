47079c1739ecf27e2ca63746893f43266dcdd0b2
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-07 20:20:47 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    string a;
+ 
+    
+    cin >> a;
+    
+    
+    
+    
+    cout << a << "??!"<< endl;
+    
+    
+}

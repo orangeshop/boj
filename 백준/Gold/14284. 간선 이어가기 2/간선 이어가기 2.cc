ae0d4048d569e9d93189362b3d11d741a68a19da
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-04-13 00:58:17 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 88 ms, Memory: 4720 KB -BaekjoonHub

---
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include<cstring>
+#include <vector>
+#include <stack>
+#include <memory.h>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <math.h>
+#include <set>
+#include <map>
+using namespace std;
+#define S second
+#define F first
+#define l long
+#define MAX 0x7ffffff
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+int ddx[8] = {-1,-2,-2,-1,1,2,2,1};
+int ddy[8] = {-2,-1,1,2,-2,-1,1,2};
+long INF = 0x3f3f3f3f;
+
+int N,M,st,en;
+vector<pair<int,int> > adj[5005];
+priority_queue<pair<int,int> , vector<pair<int,int> >, greater<pair<int, int> > > pq;
+int d[5005];
+
+
+int main(){
+    cin >> N >> M;
+
+    for(int i=0; i<M; i++){
+        int u = 0;
+        int w = 0;
+        int c = 0;
+
+        cin >> u >> w >> c;
+
+        adj[u].push_back(make_pair(c, w));
+        adj[w].push_back(make_pair(c, u));
+        
+    }
+
+    cin >> st >> en;
+
+    fill(d,d+N+1, INF);
+
+    d[st] = 0;
+
+    pq.push(make_pair(d[st], st));
+
+    while(!pq.empty()){
+        pair<int,int> cur = pq.top();
+        pq.pop();
+
+        if(d[cur.second] != cur.first)continue;
+
+        for(auto nxt : adj[cur.second]){
+            if(d[nxt.second] <= d[cur.second]+ nxt.first) continue;
+
+            d[nxt.second] = d[cur.second] + nxt.first;
+
+            pq.push(make_pair(d[nxt.second], nxt.second));
+        }
+    }
+
+
+    cout << d[en] << endl;
+}

19fc7caf50d977bc0aba4ff667c84ab3f94b434b
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-07-28 15:23:52 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(int k, vector<int> score) {
+    vector<int> answer;
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    for(int i=0; i<score.size(); i++){
+        pq.push(score[i]);
+        if(pq.size() > k){
+            pq.pop();
+        }
+        cout << pq.top() << endl;
+        answer.push_back(pq.top());
+    }
+    
+    return answer;
+}

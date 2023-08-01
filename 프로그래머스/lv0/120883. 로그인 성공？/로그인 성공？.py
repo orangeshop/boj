8e3c14dc30b958f697ba3edf81f2c8811043e74f
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-08-01 16:02:55 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,10 @@
+def solution(id_pw, db):
+    answer = 'fail'
+    
+    for i in range(len(db)):
+        if(id_pw[0] == db[i][0]):
+            if(id_pw[1] == db[i][1]):
+                answer = 'login'
+            else:
+                answer = 'wrong pw'
+    return answer

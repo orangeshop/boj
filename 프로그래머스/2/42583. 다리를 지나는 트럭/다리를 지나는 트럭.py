1dfc0a67d81f508932cff7209e1801694a03061f
orangeshop <81177568+orangeshop@users.noreply.github.com>
2023-10-27 22:11:11 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -12,35 +12,24 @@ def solution(bridge_length, weight, truck_weights):
     
     result_list = []
     
-    print(len(result_list))
-    print(len(truck_weights))
-    
-    cnt = 0
     idx = 0
     while(True):
         if len(result_list) == len(truck_weights):
             break
-        # if(cnt == 14):
-        #     break
-        # print(f'{deque_bridge_now_weight} {deque_bridge_now_idx} {idx}')
-        
+            
         if len(deque_bridge_now_idx) != 0:
-             # print(f"{idx} {deque_bridge_now_idx[0]} {bridge_length}")
+            
             if idx - deque_bridge_now_idx[0] >= bridge_length:
-                # print("hi")
+                
                 deque_bridge_now_idx.popleft()
                 deque_bridge_now_weight.popleft()
                 result_list.append(-1)
-                # idx += 1
                 
         if len(deque_truck_weights) != 0:
             if sum(deque_bridge_now_weight) + deque_truck_weights[0] <= weight:
                 deque_bridge_now_weight.append(deque_truck_weights[0])
                 deque_truck_weights.popleft()
-                # result_list.append(-1)
                 deque_bridge_now_idx.append(idx)
-        
-        cnt += 1
         idx += 1
         
         

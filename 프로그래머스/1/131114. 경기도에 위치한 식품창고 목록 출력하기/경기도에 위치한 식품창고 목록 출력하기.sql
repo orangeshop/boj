69e4d6c9ac9823004cd6ba586febb3725a670f96
orangeshop <81177568+orangeshop@users.noreply.github.com>
2024-01-28 22:14:19 +0900

    [level 1] Title: 경기도에 위치한 식품창고 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT WAREHOUSE_ID
+        , WAREHOUSE_NAME
+        , ADDRESS
+        # ,FREEZER_YN
+        , ifnull(FREEZER_YN, 'N') as FREEZER_YN
+from FOOD_WAREHOUSE
+where WAREHOUSE_NAME like '%경기%'
+order by WAREHOUSE_ID
+;

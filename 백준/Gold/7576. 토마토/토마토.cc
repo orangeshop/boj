5e7b5bbc382ae50596f7d0aedbe3589886a87549
최영호 <81177568+orangeshop@users.noreply.github.com>
2024-07-17 23:31:16 +0900

    [Gold V] Title: 토마토, Time: 244 ms, Memory: 15064 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,85 +1,97 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by 최영호 on 7/8/24.
+//
+
 #include <iostream>
-#include <string>
-#include <vector>
-#include <stack>
 #include <algorithm>
+#include <stack>
+#include <string>
+#include <sstream>
 #include <queue>
+
 using namespace std;
 
-int board[1000][1000];
-bool vis[1000][1000];
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
-int n=0;
-int m=0;
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
 
-int main()
-{
-    queue<pair<int,int>> Q;
+
+int M,N;
+int board[1005][1005];
+bool vis[1005][1005];
+int cnt[1005][1005];
+
+int ans;
+
+queue<pair<int, int>> Q;
+
+int main(){
+    cin >> M >> N;
     
-    cin >> m >> n;
-    
-    for(int i=0; i<n; i++)
-    {
-        for(int k=0; k<m; k++)
-        {
+    for(int i =0; i < N; i++){
+        for(int k =0; k < M; k++){
             cin >> board[i][k];
-            if(board[i][k]==1)
-            {
+            if(board[i][k] == 1){
                 Q.push({i,k});
-                vis[i][k] =1;
+                vis[i][k] = true;
             }
-            
         }
     }
     
-    int cnt =0;
     
-
-        //cout << A.first << " " << A.second << endl;
-        
-        
-        while(!Q.empty())
-        {
-            pair<int,int> cur = Q.front(); Q.pop();
-        
-            //cout << cur.first <<"," << cur.second << "->";
-            for(int i=0; i<4; i++)
-            {
-                int nx = dx[i] + cur.first;
-                int ny = dy[i] + cur.second;
+    
+    bool check = false;
+    
+    for(int i =0; i < N; i++){
+        for(int k =0; k < M; k++){
+            if(board[i][k] == 0){
+                check = true;
+            }
+        }
+    }
+    
+    if(check == true){
+        while(!Q.empty()){
+            pair<int,int> cur = Q.front();
+            Q.pop();
             
-                if(board[nx][ny]==-1) continue;
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if(vis[nx][ny] == 1) continue;
+            for(int dir = 0; dir < 4; dir++){
+                int nx = dx[dir] + cur.first;
+                int ny = dy[dir] + cur.second;
                 
-                board[nx][ny] = board[cur.first][cur.second]+1;
-                cnt = board[nx][ny];
+                if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+                if(board[nx][ny] == -1 || vis[nx][ny] == true) continue;
+                
+                cnt[nx][ny] = cnt[cur.first][cur.second] + 1;
+                
+                vis[nx][ny] = true;
+                
                 Q.push({nx,ny});
-                vis[nx][ny] =1;
-            }
-           
-        
-    }
-    
-    for(int i=0; i<n; i++)
-    {
-        for(int k=0; k<m; k++)
-        {
-            //cout << board[i][k] << " ";
-            if(board[i][k]==0)
-            {
-                cnt = -1;
+                
             }
         }
-        //cout << endl;
+    }else{
+        cout << 0 << endl;
+        return 0;
     }
     
-    if(cnt<=0)
-    {
-        cout << cnt << endl;
+    
+    for(int i =0; i < N; i++){
+        for(int k =0; k < M; k++){
+            if(vis[i][k] == false && board[i][k] != -1){
+                cout << -1 << endl;
+                return 0;
+            }
+            
+            ans = max(ans, cnt[i][k]);
+        }
     }
-    else{
-    cout << cnt-1 << endl;
-    }
+    
+    cout << ans << endl;
+    
+    
+    
+    
 }

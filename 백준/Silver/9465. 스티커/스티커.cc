5902d7f3c468a3574c539a2d6e2495a93caf7f92
최영호 <81177568+orangeshop@users.noreply.github.com>
2024-08-07 00:01:10 +0900

    [Silver I] Title: 스티커, Time: 256 ms, Memory: 3584 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -1,64 +1,73 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by 최영호 on 7/8/24.
+//
+
 #include <iostream>
-#include <string>
-#include <vector>
-#include <stack>
 #include <algorithm>
+#include <stack>
+#include <string>
+#include <sstream>
 #include <queue>
-#include <tuple>
+#include <set>
+
 using namespace std;
 
-int s[4][100005];
-int d[4][100005];
-int n;
 
-int main()
-{
-    int a=0;
-    vector<int> V;
-    cin >> a;
+int T;
+
+int d[2][100005];
+int dp[2][100005];
+
+int main(){
+    cin >> T;
     
-    for(int i=0; i<a; i++)
-    {
+    for(int i =0; i < T; i++){
+        int n = 0;
         cin >> n;
-    
-        for(int i =1; i<3; i++)
-        {
-            for(int k=0; k<n; k++)
-            {
-                cin >> s[i][k];
-            }
-        }
-    
-   
-        d[1][1] = max(d[1][1],d[2][1]);
-        d[2][1] = min(d[1][1],d[2][1]);
-        for(int i =0; i<n; i++)
-        {
         
-            d[1][i+2] = d[1][i] + max(s[1][i+2],s[2][i+2]);
-            d[2][i+2] = d[2][i] + max(s[1][i+2],s[2][i+2]);
-        
-        
-            d[2][i+1] = max(d[2][i] , d[1][i] + s[1][i]);
-            d[1][i+1] = max(d[1][i] , d[2][i] + s[2][i]);
-       
-        }
-        
-    
-        //cout << max(d[1][n], d[2][n]) << endl;
-        V.push_back(max(d[1][n], d[2][n]));
-        for(int i =1; i<3; i++)
-        {
-            for(int k=0; k<n; k++)
-            {
+        for(int i =0; i < 2; i++){
+            for(int k =0; k < n; k++){
                 d[i][k] = 0;
+                dp[i][k] = 0;
             }
         }
+        
+        for(int i =0; i < n; i++){
+            cin >> d[0][i];
+        }
+        
+        for(int i =0; i < n; i++){
+            cin >> d[1][i];
+        }
+        
+        dp[0][0] = d[0][0];
+        dp[1][0] = d[1][0];
+        
+        dp[0][1] = d[1][0] + d[0][1];
+        dp[1][1] = d[0][0] + d[1][1];
+        
+        for(int i = 2; i <= n; i++){
+            dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + d[0][i];
+            dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + d[1][i];
+            
+//            cout << dp[0][i] << " " << dp[1][i] << endl;
+        }
+        
+//        for(int i =0; i < 2; i++){
+//            for(int k =0; k < n; k++){
+//                cout << dp[i][k] << " ";
+//            }
+//            cout << endl;
+//        }
+        
+        
+        cout << max(dp[0][n-1], dp[1][n-1]) << endl;
+        
     }
     
-    for(int i=0; i<a; i++)
-    {
-        cout << V[i]<<'\n';
-    }
+    
     
 }

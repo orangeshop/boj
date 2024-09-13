13cc4267791fab3ea8036bd57d770c295a177263
최영호 <81177568+orangeshop@users.noreply.github.com>
2024-09-13 22:14:17 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 1060 ms, Memory: 70512 KB -BaekjoonHub

---
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.cc
@@ -0,0 +1,58 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by 최영호 on 7/8/24.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <set>
+#include <map>
+#include <cstring>
+using namespace std;
+
+int N;
+vector<int> adj[1000005];
+
+bool vis[1000005];
+
+int dp[1000005][2];
+
+void dfs(int node){
+    vis[node] = true;
+    
+    dp[node][0] = 0;
+    dp[node][1] = 1;
+    
+    for(int nxt : adj[node]){
+        if(vis[nxt] == true) continue;
+        dfs(nxt);
+        dp[node][0] += dp[nxt][1];
+        dp[node][1] += min(dp[nxt][0], dp[nxt][1]);
+        
+    }
+}
+
+int main(){
+    cin >> N;
+    
+    for(int i =0; i < N-1; i++){
+        int a = 0;
+        int b = 0;
+        
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    
+    dfs(1);
+    
+    cout << min(dp[1][0], dp[1][1]);
+    
+    
+}

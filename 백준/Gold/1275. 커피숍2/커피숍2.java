066710cc56f16a7b04163575b96bd42fe48c81d6
최영호 <81177568+orangeshop@users.noreply.github.com>
2024-10-22 23:51:12 +0900

    [Gold I] Title: 커피숍2, Time: 800 ms, Memory: 85496 KB -BaekjoonHub

--- 백준/Gold/1275. 커피숍2/커피숍2.java
+++ 백준/Gold/1275. 커피숍2/커피숍2.java
@@ -1,111 +1,81 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static void init(long a[], long tree[], int node, int start, int end) {
-		if (start == end) {
-			tree[node] = a[start];
-		}else {
-			init(a, tree, node * 2, start, (start + end) / 2);
-			init(a, tree, node * 2 + 1, (start + end) / 2 + 1, end);
-			tree[node] = tree[node * 2] + tree[node * 2 + 1];
-		}
-	}
-	
-	static long query(long tree[], int node, int start, int end, int left, int right) {
-		if (left > end || right < start) {
-			return 0;
-		}
-		if (left <= start && end <= right) {
-			return tree[node];
-		}
+
+	static long[] elements, tree;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int q = Integer.parseInt(st.nextToken());
 		
-		long lsum = query(tree, node * 2, start, (start + end) / 2, left, right);
-		long rsum = query(tree, node * 2 + 1, (start + end) / 2 + 1, end , left, right);
-		return lsum + rsum;
-	}
-	
-	static void update(long a[], long tree[], int node, int start, int end, int index, long val) {
-		if (index < start || index > end) {
-			return;
+		elements = new long[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			elements[i] = Integer.parseInt(st.nextToken());
 		}
-		if (start == end) {
-			a[index] = val;
-			tree[node] = val;
-			return;
+		int size = getTreeSize(n);
+		tree = new long[size];
+		init(0,n-1,1);
+
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken())-1;
+			int y = Integer.parseInt(st.nextToken())-1;
+			
+			if(x>y) {
+				int tmp =x;
+				x = y;
+				y = tmp; 
+			}
+			int idx = Integer.parseInt(st.nextToken())-1;
+			int value = Integer.parseInt(st.nextToken());
+			
+			long pSum = prefixSum(0, n-1, 1, x, y);
+			sb.append(pSum+"\n");
+			
+			long dif = value - elements[idx];
+			update(0, n-1, 1, idx, dif);
+			elements[idx] = value;
 		}
+		System.out.println(sb.toString());
 		
-		update(a, tree, node * 2, start, (start + end) /2 , index, val);
-		update(a, tree, node * 2 + 1, (start + end) / 2 + 1, end, index, val);
-		tree[node] = tree[node * 2] + tree[node * 2 +1];
 	}
-	
 	
 	static int getTreeSize(int n) {
 		int h = (int)Math.ceil(Math.log(n)/Math.log(2));
 		return (int) Math.pow(2, h+1);
 	}
 	
-	static int N;
-	static int Q;
-	static long[] element, tree;
-	
-	
-	
-	
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		Q = Integer.parseInt(st.nextToken());
-		
-		element = new long[N];
-		
-		st = new StringTokenizer(br.readLine());
-		
-		for (int i = 0; i < N; i++) {
-			element[i] = Integer.parseInt(st.nextToken());
+	static long init(int s, int e, int node) {
+		if(s == e) {
+			return tree[node] = elements[s];
 		}
-		
-		int size = getTreeSize(N);
-		
-		tree = new long[size];
-		init(element, tree,1, 0,N-1);
-		
-		StringBuilder sb = new StringBuilder();
-		
-		for(int i =0; i < Q; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken())-1;
-			int y = Integer.parseInt(st.nextToken())-1;
-			
-			if (x > y) {
-				int tmp = x;
-				x = y;
-				y = tmp;
-			}
-			
-			int idx = Integer.parseInt(st.nextToken())-1;
-			int val = Integer.parseInt(st.nextToken());
-			
-			long pSum = query(tree,1, 0, N-1, x,y);
-			sb.append(pSum+"\n");
-			
-//			long dif = val - element[idx];
-			update(element, tree, 1, 0, N-1, idx, val);
-//			element[idx] = val;
-		}
-		
-		System.out.println(sb.toString());
-		
-		
-		
-		
+		int mid = (s+e)/2;
+		return tree[node] = init(s, mid, node*2)+init(mid+1, e, node*2+1);
 	}
-
+	
+	static void update(int s, int e, int node, int idx, long dif) {
+		if(s <= idx && idx <= e) {
+			tree[node] += dif;
+		}else return;
+		
+		if(s == e) return;
+		
+		int mid = (s+e)/2;
+		update(s, mid, node*2, idx, dif);
+		update(mid+1, e, node*2+1, idx, dif);
+	}
+	
+	static long prefixSum(int s, int e, int node, int l, int r) {
+		if(e < l || r < s) return 0;
+		if(l <= s && e <= r) {
+			return tree[node];
+		}
+		
+		int mid = (s+e)/2;
+		return prefixSum(s, mid, node*2, l, r)+ prefixSum(mid+1, e, node*2+1, l, r);
+	}
 }

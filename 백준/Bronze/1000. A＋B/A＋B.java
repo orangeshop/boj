3a380bf52baecc89d6036146fe71d30ddc442216
최영호 <81177568+orangeshop@users.noreply.github.com>
2025-04-02 17:29:32 +0900

    [Bronze V] Title: A+B, Time: 112 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        BiFunction<Integer, Integer, Integer> add = (x, y) -> x + y;
+
+        System.out.println(add.apply(a, b));
+    }
+}

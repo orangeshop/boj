53e75de1296e42266f46ba230f5d2fd69db01b91
최영호 <81177568+orangeshop@users.noreply.github.com>
2025-10-14 16:50:29 +0900

    [level 2] Title: 광물 캐기, Time: 81.41 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -6,97 +6,70 @@ class Solution {
     
     static int answer = Integer.MAX_VALUE;
     static int[] arr = new int[15];
-    static int pickSize = 0;
+    
     static int mineralsSize = 0;
-    static int[] picksTmp = new int[3];
-    static String[] mineralsTmp;
-     
     
-    static int[][] mineBoard = {{1,1,1},{5,1,1},{25,5,1}};
+    static int[] picksGlobal;
+    static String[] mineralsGlobal;
     
+    static Map<String, Integer> m = new HashMap<>();
+
+
+    static int[][] mineBoard = {{1, 1, 1}, {5, 1, 1}, {25, 5, 1}};
+
     public int solution(int[] picks, String[] minerals) {
-        
-        for(int i =0; i < picks.length; i++){
-            picksTmp[i] = picks[i];
-        }
-        
-        pickSize = picks.length;
+
         mineralsSize = minerals.length;
-        
-        mineralsTmp = new String[minerals.length];
-        
-        for(int i =0; i < minerals.length; i++){
-            mineralsTmp[i] = minerals[i];
-        }
+
+        picksGlobal = picks.clone();
+        mineralsGlobal = minerals.clone();
 
         Arrays.fill(arr, -1);
-        
-        solve();
+
+        m.put("diamond", 0);
+        m.put("iron", 1);
+        m.put("stone", 2);
+
+        dfs(0, 0, 0, 0);
+
         return answer;
     }
-    
-    public static void dfs(int depth, int stone, int iron, int dia){
-        /*
-        minerals가 최대 50
-        
-        곡괭이 하나당 5
-        
-        즉 depth가 10
-        
-        곡괭이를 다 쓰거나 or 곡괭이 * 5의 값이 미내랄의 개수보다 커질 경우
-        
-        3이 돌
-        2가 철
-        1이 다이야
-        
-        */
-        
-        if((stone == picksTmp[2] && iron == picksTmp[1] && dia == picksTmp[0]) || depth * 5 >= mineralsSize){
-            
+
+    public static void dfs(int depth, int stone, int iron, int dia) {
+
+        if ((stone == picksGlobal[2] && iron == picksGlobal[1] && dia == picksGlobal[0]) || depth * 5 >= mineralsSize) {
+
             int result = 0;
             int idx = 0;
-            
-            for(int i =0; i < mineralsSize; i++){
-                if(i > 0 && i % 5 == 0){ 
+
+            for (int i = 0; i < mineralsSize; i++) {
+                if (i > 0 && i % 5 == 0) {
                     idx++;
+                    if (arr[idx] == -1) break;
                 }
-                if(arr[idx] == -1) break;
-                
-                int num = 0;
-                if(mineralsTmp[i].equals("diamond")) num = 0;
-                if(mineralsTmp[i].equals("iron")) num = 1;
-                if(mineralsTmp[i].equals("stone")) num = 2;
-                
-                result += mineBoard[arr[idx]][num];   
+                result += mineBoard[arr[idx]][m.get(mineralsGlobal[i])];
             }
             answer = Math.min(result, answer);
-            
+
             return;
         }
-        
-        if(stone < picksTmp[2]){
+
+        if (stone < picksGlobal[2]) {
             arr[depth] = 2;
             dfs(depth + 1, stone + 1, iron, dia);
             arr[depth] = -1;
         }
-        
-        if(iron < picksTmp[1]){
+
+        if (iron < picksGlobal[1]) {
             arr[depth] = 1;
             dfs(depth + 1, stone, iron + 1, dia);
             arr[depth] = -1;
         }
-        
-        if(dia < picksTmp[0]){
+
+        if (dia < picksGlobal[0]) {
             arr[depth] = 0;
             dfs(depth + 1, stone, iron, dia + 1);
             arr[depth] = -1;
-        }    
+        }
     }
-    
-    
-    
-    public static void solve(){
-        dfs(0,0,0,0);
-    }
-    
 }

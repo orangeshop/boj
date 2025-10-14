85cf97af9c1e77c994b9c24609871ab87a9fcf38
최영호 <81177568+orangeshop@users.noreply.github.com>
2025-10-14 22:43:18 +0900

    [Silver III] Title: N과 M (7), Time: 776 ms, Memory: 319460 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int[] arrays = new int[M];
+        boolean[] vis = new boolean[N];
+        dfs(0, arrays, vis);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void dfs(int depth, int[] arrays, boolean[] vis) {
+        if(depth == M) {
+            String str = "";
+            for(int i = 0; i < M; i++) {
+                str += arrays[i] + " ";
+            }
+            sb.append(str.trim());
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+//            if(vis[i]) continue;
+            arrays[depth] = arr[i];
+            vis[i] = true;
+            dfs(depth + 1, arrays, vis);
+            arrays[depth] = 0;
+            vis[i] = false;
+        }
+    }
+}

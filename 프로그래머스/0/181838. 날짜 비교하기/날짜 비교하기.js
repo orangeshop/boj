fcb2a6bbbd17ede397973b2566d471143c971043
최영호 <81177568+orangeshop@users.noreply.github.com>
2025-10-31 15:52:59 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,28 @@
+function solution(date1, date2) {
+    var answer = 0;
+    
+    if(date1[0] === date2[0]){
+        if(date1[1] === date2[1]){
+            if(date1[2] === date2[2]){
+                answer = 0;
+            }else{
+                // 일 비교
+                if(date1[2] < date2[2]){
+                    answer = 1;
+                }
+            }
+        }else{
+            // 월 비교
+            if(date1[1] < date2[1]){
+                answer = 1;
+            }
+        }
+    }else{
+        // 년도 비교
+        if(date1[0] < date2[0]){
+            answer = 1;
+        }
+    }
+    
+    return answer;
+}

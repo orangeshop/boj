1b23a854477e269d64114c9927d6ad9dafa234db
최영호 <81177568+orangeshop@users.noreply.github.com>
2026-02-19 16:19:29 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,62 +1,134 @@
 -- 코드를 입력하세요
-with af_table as(
-    select 
-    0 as num
-    union select 
-    1
-    union select 
-    2
-    union select 
-    3
-    union select 
-    4
-    union select 
-    5
-    union select 
-    6
-    union select 
-    7
-    union select 
-    8
-    union select 
-    9
-    union select 
-    10
-    union select 
-    11
-    union select 
-    12
-    union select 
-    13
-    union select 
-    14
-    union select 
-    15
-    union select 
-    16
-    union select 
-    17
-    union select 
-    18
-    union select 
-    19
-    union select 
-    20
-    union select 
-    21
-    union select 
-    22
-    union select 
-    23
+with t1 as(
+    select 0 as HOUR, 0 as COUNT
+    from dual
+    
+    UNION ALL 
+    
+    select 1 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 2 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 3 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 4 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 5 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 6 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 7 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 8 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 9 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 10 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 11 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 12 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 13 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 14 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 15 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 16 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 17 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 18 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 19 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 20 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 21 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 22 , 0
+    from dual
+    
+    UNION ALL 
+    
+    select 23 , 0
+    from dual
+), t2 as(
+    select HOUR(DATETIME) as HOUR, count(*) as count
+    from ANIMAL_OUTS
+    group by HOUR(DATETIME)
 )
 
 
+select a.HOUR, 
+case 
+    when a.count + b.count is null then 0 
+    else a.count + b.count
+end as COUNT
+from t1 a left join t2 b
+on a.hour = b.hour
 
-select 
-num
-, (select count(*) from ANIMAL_OUTS where num = HOUR(DATETIME)) as COUNT
-from af_table
-group by num
-
-
-;
